7dd2d06e76a83e318f4dea20fb88d9e065828d44
Ezra <ezrakissous26@gmail.com>
2026-07-05 10:56:08 +0300

    add loading fromm link to my local json

--- race-service.js
+++
@@ -1,7 +0,0 @@
-
-
-function getall() {
-    fetch("https://server-for-test-week-13.onrender.com/api/race")
-}
-
-getall()
---
+++ server-load.js
@@ -0,0 +1,25 @@
+const fs = require('fs')
+
+async function getData() {
+    try {
+        const response = await fetch('https://server-for-test-week-13.onrender.com/api/race')
+        const result = await response.json()
+        return result
+    } catch (err) {
+        console.error(err.message)
+    }
+}
+
+async function writeToJson() {
+    try {
+        const response = await fetch('https://server-for-test-week-13.onrender.com/api/race')
+        let result = await getData()
+        await fs.writeFile('./data.json', JSON.stringify(result, null, 2), (err) => {
+            if (err) throw err;
+        })
+    } catch (err) {
+        console.error(err.message)
+    }
+}
+
+writeToJson()
