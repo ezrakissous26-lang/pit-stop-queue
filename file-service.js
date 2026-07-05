298daa130cd39889865ca82c47e7751fdb2127c5
Ezra <ezrakissous26@gmail.com>
2026-07-05 11:04:38 +0300

    add new file for managing the local json

---
+++ file-service.js
@@ -0,0 +1,10 @@
+const fs = require('fs')
+
+function readFile(callback) {
+    return new Promise((resolve, reject) => {
+        fs.readFile('./data.json', 'utf-8', (err, data) => {
+            if (err) reject(err);
+            else resolve(JSON.parse(data))
+        })
+    })
+}
